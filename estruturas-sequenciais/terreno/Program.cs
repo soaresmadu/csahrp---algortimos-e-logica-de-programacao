7e68dafcda692c0981f8f5b17ee1e0aad96bf7ec
soaresmadu <eduardajssoares@gmail.com>
2024-11-23 16:24:16 -0300

    Add files via upload

---
+++ estruturas-sequenciais/terreno/Program.cs
@@ -0,0 +1,27 @@
+﻿//Fazer um programa para ler as medidas da largura e comprimento de um terreno retangular com uma casa decimal, bem como o valor do metro quadrado do terreno com duas casas decimais. Em seguida, o programa deve mostrar o valor da área do terreno, bem como o valor do preço do terreno, ambos com duas casas decimais, conforme exemplo. 
+
+using System;
+using System.Globalization; 
+
+namespace Programa {
+    class Program {
+    static void Main(string[] args) {
+
+        CultureInfo CI = CultureInfo.InvariantCulture;
+        double largura, comprimento, valorMetro, area, preco;
+
+        Console.Write("Digite a largura do terreno: ");
+        largura = double.Parse(Console.ReadLine(), CI);
+        Console.Write("Digite o comprimento do terreno: ");
+        comprimento = double.Parse(Console.ReadLine(), CI);
+        Console.Write("Digite o valor do metro quadrado: ");
+        valorMetro = double.Parse(Console.ReadLine(), CI);
+
+        area = largura * comprimento;
+        preco = area * valorMetro;
+
+        Console.WriteLine("Área do terreno: " + area.ToString("F2", CI));
+        Console.WriteLine("Preço do terreno: " + preco.ToString("F2", CI));
+ }
+ }
+} 
---
+++ estruturas-sequenciais/troco/Program.cs
@@ -0,0 +1,26 @@
+﻿//Fazer um programa para calcular o troco no processo de pagamento de um produto de uma mercearia. O programa deve ler o preço unitário do produto, a quantidade de //unidades compradas deste produto, e o valor em dinheiro dado pelo cliente (suponha que haja dinheiro suficiente). Seu programa deve mostrar o valor do troco a ser devolvido ao cliente. 
+
+using System;
+using System.Globalization; 
+
+namespace Programa {
+    class Program {
+    static void Main(string[] args) {
+
+        CultureInfo CI = CultureInfo.InvariantCulture;
+        double preco, recebido, troco;
+        int qtd;
+
+        Console.Write("Preço unitário do produto: ");
+        preco = double.Parse(Console.ReadLine(), CI);
+        Console.Write("Quantidade comprada: ");
+        qtd = int.Parse(Console.ReadLine());
+        Console.Write("Dinheiro recebido: ");
+        recebido = double.Parse(Console.ReadLine(), CI);
+
+        troco = recebido - (preco * qtd);
+
+        Console.Write("TROCO = " + troco.ToString("F2", CI));
+ }
+ }
+} 
